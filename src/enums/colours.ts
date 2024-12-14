7314005f3ce578e98e5f19e6189bbdd7f870fd31
Mohammad Kawadri <62560333+mtkwebdev@users.noreply.github.com>
2024-12-14 21:38:11 +0000

    Enums: added colours

---
+++ src/enums/colours.ts
@@ -0,0 +1,9 @@
+import { type WidgetSelectedColor } from "@/types/productWidgets/types";
+
+export const COLOURS: Array<WidgetSelectedColor> = [
+  "blue",
+  "green",
+  "beige",
+  "white",
+  "black",
+];
